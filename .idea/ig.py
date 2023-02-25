1ea73aae78d739e3a3c914ec7488b9123dc1e68b
user <arkadiychulkov11@gmail.com>
2023-02-25 11:03:31 +0200

    sdsdsddddffffqwertyuiop[]';lkjhgfdsazxcvbnm,./1233334444444567890000-='+-/*

--- .idea/ig.py
+++ .idea/ig.py
@@ -1,9 +1,17 @@
-def xz(x):
-    c = 1
-    while c <= x:
-        yield c
-        c += 1
-f = xz(1333)
-for i in range(200):
+def xz():
+    s = input("do you want to DO coordinate graphik: yes or no ")
+    while True:
+        if s == "yes":
+            x = int(input("vertical high"))
+            c = 1
+            while c <= x:
+                yield c
+                c += 1
+            break
+        else:
+                print("go away I.F.Y.M.B.D.F.G.H.A")
+                continue
+f = xz()
+for i in range(int(input("horizontal high"))):
     print(f.__next__())
 print(list(f))
